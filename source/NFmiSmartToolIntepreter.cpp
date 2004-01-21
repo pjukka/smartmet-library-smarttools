5d63b08fc8de21ee01f45db5297812026c1ee89f
pietarin <pietarin>
2004-01-21 08:39:16 +0000

    Lisäsin ne stdafx-includet takaisin nyt kommentteihin ja laitoin siihen liittyvät DEBUG_NEW systeemit kommenttiin. Kyseinen koodi on muistivuotojen metsästystä varten MSVC ympäristössä.

--- source/NFmiSmartToolIntepreter.cpp
+++ source/NFmiSmartToolIntepreter.cpp
@@ -45,14 +45,14 @@
 #include <cctype>
 
 using namespace std;
-
-
+/*
+#include "stdafx.h"
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
 static char THIS_FILE[] = __FILE__;
 #endif
-
+*/
 // globaali tarkistus luokka etsim‰‰n rivin lopetusta
 struct EndOfLineSearcher
 {
--- source/NFmiSmartToolModifier.cpp
+++ source/NFmiSmartToolModifier.cpp
@@ -49,13 +49,14 @@
 #include <stdexcept>
 
 using namespace std;
-
+/*
+#include "stdafx.h"
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
 static char THIS_FILE[] = __FILE__;
 #endif
-
+*/
 
 NFmiSmartToolCalculationBlockVector::NFmiSmartToolCalculationBlockVector(void)
 :itsCalculationBlocks()
