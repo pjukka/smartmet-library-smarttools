74487a363b6525c9e82eb5100dca472fae53f6d9
pietarin <pietarin>
2005-09-13 10:03:10 +0000

    Poistin DEBUG_NEW eli "vuodon etsintä" määritykset turhina.

--- source/NFmiSmartToolCalculation.cpp
+++ source/NFmiSmartToolCalculation.cpp
@@ -37,13 +37,6 @@
 #include <stdexcept>
 
 
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
-
-
 using namespace std;
 
 //--------------------------------------------------------
