baffa463f437a3cf78ae1deec827f386c62e3425
pietarin <pietarin>
2006-10-11 14:27:42 +0000

    Lisäsin MetEditorin käyttämän XY-pistecache luokan smarttools-kirjastoon. Luokka osaa cachettaa ns. grid-stringin avulla erilaisten hilojen piste-matriisia. Grid-stringi koostuu projektio stringistä ja hila koosta.

---
+++ include/NFmiGridPointCache.h
@@ -0,0 +1,42 @@
+//© Ilmatieteenlaitos/Marko.
+//Original 09.10.2006
+//
+// Luokka pitää MetEditoriin imagine contourauksessa käytettyjen
+// xy-pisteiden cachea.
+//---------------------------------------------------------- NFmiGridPointCache.h
+
+#ifndef __NFMIGRIDPOINTCACHE_H__
+#define __NFMIGRIDPOINTCACHE_H__
+
+#include "NFmiDataMatrix.h"
+#include "NFmiPoint.h"
+
+#include <string>
+#include <map>
+
+class NFmiGridPointCache
+{
+public:
+
+	struct Data
+	{
+		NFmiPoint itsOffSet; // koska lasketut hilat lasketaan (usean ruudukon karttanäytössä) eri kohdissa 0,0 - 1,1 maailmaa, pitää
+							// 'originaalin' hilan offset origoon laittaa talteen, että myöhemmin voidaan laskea haluttuja offsetteja contoureille
+		NFmiDataMatrix<NFmiPoint> itsPoints;
+	};
+
+	typedef std::map<std::string, Data> pointMap;
+
+	NFmiGridPointCache(void):itsPointCache(){}
+	~NFmiGridPointCache(void){}
+	void Add(const std::string &theGridStr, const NFmiGridPointCache::Data &theData);
+	pointMap::iterator Find(const std::string &theGridStr) {return itsPointCache.find(theGridStr);};
+	pointMap::iterator End(void) {return itsPointCache.end();};
+	void Clear(void) {itsPointCache.clear();};
+
+
+private:
+	pointMap itsPointCache;
+};
+
+#endif // __NFMIGRIDPOINTCACHE_H__
---
+++ source/NFmiGridPointCache.cpp
@@ -0,0 +1,20 @@
+//© Ilmatieteenlaitos/Marko.
+//Original 09.10.2006
+//
+// Luokka pitää MetEditoriin imagine contourauksessa käytettyjen
+// xy-pisteiden cachea.
+//---------------------------------------------------------- NFmiGridPointCache.cpp
+
+#include "NFmiGridPointCache.h"
+
+void NFmiGridPointCache::Add(const std::string &theGridStr, const NFmiGridPointCache::Data &theData)
+{
+	pointMap::iterator it = itsPointCache.find(theGridStr);
+	if(it == itsPointCache.end())
+		itsPointCache.insert(std::make_pair(theGridStr, theData));
+	else // jos löytyi jo cachesta, korvataan arvot
+	{
+		(*it).second = theData;
+	}
+}
+
