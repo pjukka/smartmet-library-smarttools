16b8cec243f7b87d7d7c368a3b7e975f07540b1d
pietarin <pietarin>
2008-04-02 15:10:41 +0000

    NFmiProducerHelperInfo -structi lisätty tänne, sitä käytetään parissa luokassa.

--- include/NFmiProducerSystem.h
+++ include/NFmiProducerSystem.h
@@ -1,11 +1,11 @@
 //© Ilmatieteenlaitos/Marko.
 //Original 20.6.2006
-// 
+//
 // Luokka pit‰‰ huolta Editoriin konffatuista malli tuottajista.
 // Voidaan konffata esim. 10 tuottajaa. Mutta editorin eri valikot ja systeemit
-// itse p‰‰tt‰v‰t kuinka montaa ensimm‰ist‰ voidaan k‰ytt‰‰. Eli Esim. Luotausn‰ytˆn 
-// tuottaja valinta radio buttoneihin saa mukaan vain kolme mallia (pit‰‰ kai tehd‰ 
-// joskus dropdown lista). Mutta esim. erilaisiin dynaamisiin parametrin valinta 
+// itse p‰‰tt‰v‰t kuinka montaa ensimm‰ist‰ voidaan k‰ytt‰‰. Eli Esim. Luotausn‰ytˆn
+// tuottaja valinta radio buttoneihin saa mukaan vain kolme mallia (pit‰‰ kai tehd‰
+// joskus dropdown lista). Mutta esim. erilaisiin dynaamisiin parametrin valinta
 // popup-valikoihin voidaan laittaa useampia tuottajia.
 //---------------------------------------------------------- NFmiWarningCenterSystem.h
 
@@ -13,6 +13,7 @@
 #define __NFMIPRODUCERSYSTEM_H__
 
 #include "NFmiProducer.h"
+#include "NFmiInfoData.h"
 #include <string>
 #include <vector>
 
@@ -44,7 +45,7 @@ private:
 	std::string itsUltraShortName; // Lyhyempi nimi esim. Hir tai Ec (k‰ytet‰‰n mm. jossain pikavalinnoissa nimen‰ ja smarttool-kielen tuottaja nimen‰ skripteiss‰)
 	std::vector<int> itsProducerIds; // useilla malleilla on vain yksi par-id, mutta esim. EC ja HIR:eilla on kaksi.
 	std::string itsDescription;
-	bool fHasRealVerticalData; // esim. UK,DWDjaUSa datoissa ei ole nyt oikeasti vertikaalidataa, vaikka siin‰ on painepintoja, n‰ist‰ malleista ei haluta 
+	bool fHasRealVerticalData; // esim. UK,DWDjaUSa datoissa ei ole nyt oikeasti vertikaalidataa, vaikka siin‰ on painepintoja, n‰ist‰ malleista ei haluta
 							   // tehd‰ luotauksia, poikkileikkausia tai trajektori laskuja, joten n‰m‰ mallit saavat arvon false.
 };
 
@@ -71,10 +72,39 @@ private:
 
 	std::vector<NFmiProducerInfo> itsProducers; // T‰h‰n voi siis laittaa tuottajia kuinka paljon tahansa,
 												// mutta kannattaa sijoittaa alkuun t‰rkeimm‰t, koska
-												// Editori p‰‰tt‰‰ eri ikkunoissa/tyˆkaluissa, kuinka monta  
+												// Editori p‰‰tt‰‰ eri ikkunoissa/tyˆkaluissa, kuinka monta
 												// tuottajaa (alkup‰‰st‰) se ottaa k‰yttˆˆn. Esim. Luotaus
 												// n‰ytˆn malli valinta radio-buttoneita on esim. vain kolme (t‰ll‰ hetkell‰)
 	std::string itsInitializeLogStr; // jos luokka initialisoidaan settingeist‰, t‰h‰n talletetaan loki stringi
+};
+
+struct NFmiProducerHelperInfo
+{
+	NFmiProducerHelperInfo(const std::string theName,	int theProducerId1,	int theProducerId2, NFmiInfoData::Type theDataType, bool groundData = true)
+	:itsName(theName)
+	,itsProducerId1(theProducerId1)
+	,itsProducerId2(theProducerId2)
+	,itsDataType(theDataType)
+	,fGroundData(groundData)
+	{}
+
+	static NFmiProducerHelperInfo MakeProducerHelperInfo(NFmiProducerInfo &theProducerInfo, NFmiInfoData::Type theType, bool groundData = true)
+	{
+		std::vector<int> prodIds = theProducerInfo.ProducerIds();
+		int prodId1 = -1;
+		if(prodIds.size() >= 1)
+			prodId1 = prodIds[0];
+		int prodId2 = -1;
+		if(prodIds.size() >= 2)
+			prodId2 = prodIds[1];
+		return NFmiProducerHelperInfo(theProducerInfo.Name(), prodId1, prodId2, theType, groundData);
+	}
+
+	std::string itsName;
+	int itsProducerId1;
+	int itsProducerId2;
+	NFmiInfoData::Type itsDataType;
+	bool fGroundData;
 };
 
 
