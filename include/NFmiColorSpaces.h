87dc9ad60a7355c876442c02ef09d6e365815037
Marko Pietarinen <marko.pietarinen@fmi.fi>
2010-12-24 12:59:29 +0200

    Korjasin GetBrighterColor-funktiota, että se vaalentaa myös värejä, joissa ei ole lightness-osaa aluksi ollenkaan.

--- include/NFmiColorSpaces.h
+++ include/NFmiColorSpaces.h
@@ -165,20 +165,6 @@ namespace NFmiColorSpaces
 
 	// theBrightningFactor on prosentti luku, jolla annettua väriä saadaan vaalennettua tai tummennettua.
 	// jos prosentti luku on > 0, vaalenee väri, jos se on < 0, tummenee väri.
-	inline NFmiColor GetBrighterColor(const NFmiColor &theColor, double theBrightningFactor)
-	{
-		RGB_color rgbCol(theColor);
-		HSL_color hslCol = NFmiColorSpaces::RGBtoHSL(rgbCol.r, rgbCol.g, rgbCol.b);
-		double lightness = hslCol.l;
-		lightness = lightness + lightness * (theBrightningFactor/100);
-
-		if(lightness > 100)
-			lightness = 100;
-		if(lightness < 0)
-			lightness = 0;
-
-		RGB_color rgbCol2 = NFmiColorSpaces::HSLtoRGB(hslCol.h, hslCol.s/100., lightness/100.);
-		return NFmiColor(rgbCol2.r/255.f, rgbCol2.g/255.f, rgbCol2.b/255.f, theColor.Alpha());
-	}
+	NFmiColor GetBrighterColor(const NFmiColor &theColor, double theBrightningFactor);
 
 }
--- source/NFmiColorSpaces.cpp
+++ source/NFmiColorSpaces.cpp
@@ -4,3 +4,21 @@
 
 #include "NFmiColorSpaces.h"
 
+NFmiColor NFmiColorSpaces::GetBrighterColor(const NFmiColor &theColor, double theBrightningFactor)
+{
+	RGB_color rgbCol(theColor);
+	HSL_color hslCol = NFmiColorSpaces::RGBtoHSL(rgbCol.r, rgbCol.g, rgbCol.b);
+	double lightness = hslCol.l;
+	if(lightness)
+		lightness = lightness + lightness * (theBrightningFactor/100.);
+	else // jos lightness oli 0, pitää vain lisätä kirkastus kerroin
+		lightness = theBrightningFactor;
+
+	if(lightness > 100)
+		lightness = 100;
+	if(lightness < 0)
+		lightness = 0;
+
+	RGB_color rgbCol2 = NFmiColorSpaces::HSLtoRGB(hslCol.h, hslCol.s/100., lightness/100.);
+	return NFmiColor(rgbCol2.r/255.f, rgbCol2.g/255.f, rgbCol2.b/255.f, theColor.Alpha());
+}
