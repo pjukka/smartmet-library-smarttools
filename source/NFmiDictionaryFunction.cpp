d0a3be8895e8e73d9ed1412200296077fa360f33
Marko Pietarinen <Marko.Pietarinen@fmi.fi>
2014-01-28 18:50:38 +0200

    Muutin GetDictionaryString -funktion toimintaa siten että se osaa hanskata tabulaattorit oikein ja että sanakirjassa \t -merkkisarja tarkoittaa oikeasti tabulaattoria.

--- source/NFmiDictionaryFunction.cpp
+++ source/NFmiDictionaryFunction.cpp
@@ -11,8 +11,12 @@ std::string GetDictionaryString(const char *theMagicWord)
 {
 	const std::string baseWords = "MetEditor::Dictionary::";
 
+    std::string usedMagicWord(theMagicWord);
+    NFmiStringTools::ReplaceAll(usedMagicWord, "\t", "\\t"); // pit‰‰ muuttaa mahdollinen tabulaattori merkkisarjaksi, miss‰ on kenoviiva ja t per‰kk‰in, koska tabulaattori on siten sanakirjassa
 	std::string finalMagicWord(baseWords);
-	finalMagicWord += theMagicWord;
-	return NFmiSettings::Optional<std::string>(finalMagicWord.c_str(), std::string(theMagicWord));
+	finalMagicWord += usedMagicWord;
+	std::string dictionarySentense = NFmiSettings::Optional<std::string>(finalMagicWord.c_str(), std::string(theMagicWord));
+    NFmiStringTools::ReplaceAll(dictionarySentense, "\\t", "\t"); // pit‰‰ muuttaa mahdolliset "\\t"-merkkijonot takaisin tabulaattoreiksi
+    return dictionarySentense;
 }
 
