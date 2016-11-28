65cf533e116951a855dc862cd1f1ba4177e597dd
Marko Pietarinen <marko.pietarinen@fmi.fi>
2016-11-28 16:01:15 +0200

    Luokka osaa nyt hanskata seuraavanlaiset resoluutio lausekkeet smarttool-kielessä:
    resolution = 12.5  // eli lausekkeet, missä haluttu laskentaresoluutio annetaan kilometreissä

--- include/NFmiExtraMacroParamData.h
+++ include/NFmiExtraMacroParamData.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include "NFmiProducerName.h"
+#include "NFmiProducer.h"
 #include "NFmiLevelType.h"
 #include "NFmiDataMatrix.h"
 
 #include "boost/shared_ptr.hpp"
 
 class NFmiFastQueryInfo;
+class NFmiInfoOrganizer;
+class NFmiArea;
 
 // Kun smarttool:ia tulkitaan, siin‰ saattaa olla osia, joita voi k‰ytt‰‰ vain macroParamien yhteydess‰.
 // T‰h‰n luokkaan talletetaan kaikkea, mit‰ extra tietoa voi lˆyty‰ annetusta skriptist‰.
@@ -14,23 +16,36 @@ class NFmiExtraMacroParamData
 {
 public:
     NFmiExtraMacroParamData();
+
+    void FinalizeData(NFmiInfoOrganizer &theInfoOrganizer);
+    bool UseSpecialResolution() const;
+    static void SetUsedAreaForData(boost::shared_ptr<NFmiFastQueryInfo> &theData, const NFmiArea *theUsedArea);
+
+    float GivenResolutionInKm() const { return itsGivenResolutionInKm; }
+    void GivenResolutionInKm(float newValue) {itsGivenResolutionInKm = newValue;}
+    const NFmiProducer& Producer() const { return itsProducer; }
+    void Producer(const NFmiProducer &newValue) { itsProducer = newValue; }
+    FmiLevelType LevelType() const { return itsLevelType; }
+    void LevelType(FmiLevelType newValue) { itsLevelType = newValue; }
+
+    boost::shared_ptr<NFmiFastQueryInfo> ResolutionMacroParamData() { return itsResolutionMacroParamData; }
+    static void AdjustValueMatrixToMissing(const boost::shared_ptr<NFmiFastQueryInfo> &theData, NFmiDataMatrix<float> &theValueMatrix);
+
 private:
+    void InitializeResolutionData(NFmiInfoOrganizer &theInfoOrganizer, float usedResolutionInKm);
+
     // Jos skriptiss‰ on annettu haluttu laskenta resoluutio tyyliin "resolution = 12.5", talletetaan kyseinen luku t‰h‰n. 
     // Jos ei ole asetettu, on arvo missing.
-    double itsGivenResolutionInKm;
+    float itsGivenResolutionInKm;
     // Jos skriptiss‰ on annettu resoluutio data muodossa tyyliin "resolution = hir_pressure",
     // talletetaan lausekkeen tuottaja ja level tyyppi n‰ihin kahteen arvoon.
-    FmiProducerName itsProducerId; // puuttuva arvo on kFmiNoProducer eli 0
+    NFmiProducer itsProducer; // puuttuva arvo on kun id = 0
     FmiLevelType itsLevelType; // puuttuva arvo on kFmiNoLevelType eli 0
 
     // t‰h‰n lasketaan datasta haluttu resoluutio  makro-parametrien laskuja varten pit‰‰ pit‰‰ yll‰ yhden 
     // hilan kokoista dataa  (yksi aika,param ja level, editoitavan datan hplaceDesc). T‰h‰n
     // dataan on laskettu haluttu resoluutio t‰m‰n macroParamin laskujen ajaksi.    
-    double itsDataBasedResolutionInKm; 
+    float itsDataBasedResolutionInKm; 
     boost::shared_ptr<NFmiFastQueryInfo> itsResolutionMacroParamData;
-    // t‰h‰n talletetaan editoitavan datan hilan suuruinen kFloatMissing:eilla
-    // alustettu matriisi ett‰ sill‰ voi alustaa makroParam dataa ennen laskuja
-    NFmiDataMatrix<float> itsResolutionMacroParamMissingValueMatrix;
-
 };
 
--- source/NFmiExtraMacroParamData.cpp
+++ source/NFmiExtraMacroParamData.cpp
@@ -1,13 +1,80 @@
 #include "NFmiExtraMacroParamData.h"
 #include "NFmiFastQueryInfo.h"
-
+#include "NFmiInfoOrganizer.h"
 
 NFmiExtraMacroParamData::NFmiExtraMacroParamData()
 :itsGivenResolutionInKm(kFloatMissing)
-,itsProducerId(kFmiNoProducer)
+,itsProducer()
 ,itsLevelType(kFmiNoLevelType)
 ,itsDataBasedResolutionInKm(kFloatMissing)
 ,itsResolutionMacroParamData()
-,itsResolutionMacroParamMissingValueMatrix()
 {
+}
+
+void NFmiExtraMacroParamData::FinalizeData(NFmiInfoOrganizer &theInfoOrganizer)
+{
+    if(itsGivenResolutionInKm != kFloatMissing)
+        InitializeResolutionData(theInfoOrganizer, itsGivenResolutionInKm);
+    //else if(itsProducer.GetIdent() != 0)
+    //{
+    //    // Alusta datapohjainen resoluutio
+    //}
+
+}
+
+bool NFmiExtraMacroParamData::UseSpecialResolution() const
+{
+    return itsResolutionMacroParamData != 0;
+}
+
+void NFmiExtraMacroParamData::SetUsedAreaForData(boost::shared_ptr<NFmiFastQueryInfo> &theData, const NFmiArea *theUsedArea)
+{
+    if(theData->Grid() && theUsedArea)
+    {
+        try
+        {
+            NFmiGrid grid(theUsedArea, theData->Grid()->XNumber(), theData->Grid()->YNumber());
+            grid.Area()->SetXYArea(NFmiRect(0, 0, 1, 1)); // t‰ss‰ pit‰‰ laittaa xy-alue 0,0 - 1,1 :ksi, koska macroParam datat zoomataan sitten erikseen
+            NFmiHPlaceDescriptor hplace(grid);
+            theData->SetHPlaceDescriptor(hplace);
+        }
+        catch(...)
+        {
+            // ei tehd‰ mit‰‰n, otetaan vain poikkeukset kiinni
+        }
+    }
+}
+
+static void CalcUsedGridSize(const NFmiArea *usedArea, int &gridSizeX, int &gridSizeY, float usedResolutionInKm)
+{
+    if(usedArea)
+    {
+        gridSizeX = boost::math::iround(usedArea->WorldXYWidth() / (usedResolutionInKm*1000.));
+        gridSizeY = boost::math::iround(usedArea->WorldXYHeight() / (usedResolutionInKm*1000.));
+    }
+}
+
+void NFmiExtraMacroParamData::InitializeResolutionData(NFmiInfoOrganizer &theInfoOrganizer, float usedResolutionInKm)
+{
+    int gridSizeX = 0;
+    int gridSizeY = 0;
+    const NFmiArea *usedArea = theInfoOrganizer.MacroParamData()->Area();
+    ::CalcUsedGridSize(usedArea, gridSizeX, gridSizeY, usedResolutionInKm);
+
+    itsResolutionMacroParamData = theInfoOrganizer.CreateNewMacroParamData(gridSizeX, gridSizeY, NFmiInfoData::kMacroParam);
+    // Pit‰‰ viel‰ s‰‰t‰‰ datan alue kartan zoomaus alueeseen. Se saadaan infoOrganizerin omasta macroParamDatasta.
+    SetUsedAreaForData(itsResolutionMacroParamData, usedArea);
+}
+
+void NFmiExtraMacroParamData::AdjustValueMatrixToMissing(const boost::shared_ptr<NFmiFastQueryInfo> &theData, NFmiDataMatrix<float> &theValueMatrix)
+{
+    if(theData)
+    {
+        const NFmiGrid *grid = theData->Grid();
+        if(grid)
+        {
+            theValueMatrix.Resize(grid->XNumber(),grid->YNumber());
+            theValueMatrix = kFloatMissing;
+        }
+    }
 }
